64f0805510b6b3fd431a37405ff8666e746cc71b
Nicolas Posa <nicoposa57@gmail.com>
2023-09-14 23:54:33 -0300

    Initial Commit

---
+++ ChatAI/ChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIApp.swift
+//  ChatAI
+//
+//  Created by Nicolas Posa on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
